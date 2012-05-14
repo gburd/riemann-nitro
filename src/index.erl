68d0eaee2dce833fc363674fd0617862d48acc5a
Gregory Burd <greg@basho.com>
2012-05-14 08:38:38 -0400

    starting to replicate the riemann-dash l&f

--- src/index.erl
+++ src/index.erl
@@ -3,33 +3,37 @@
 -compile(export_all).
 -include_lib("nitrogen_core/include/wf.hrl").
 
-main() -> #template { file="./site/templates/bare.html" }.
+main() ->
+    case wf:role(managers) of
+        true ->
+            #template { file="./site/templates/bare.html" };
+        false ->
+            wf:redirect_to_login("/login")
+    end.
 
-title() -> "Welcome to Nitrogen".
+title() -> "Dashboard".
+
+iso_8601_fmt(DateTime) ->
+    {{Year,Month,Day},{Hour,Min,Sec}} = DateTime,
+    io_lib:format("~4.10.0B-~2.10.0B-~2.10.0B ~2.10.0B:~2.10.0B:~2.10.0B",
+        [Year, Month, Day, Hour, Min, Sec]).
 
 body() ->
-    #container_12 { body=[
-        #grid_8 { alpha=true, prefix=2, suffix=2, omega=true, body=inner_body() }
-    ]}.
-
-inner_body() -> 
     [
-        #h1 { text="Welcome to Nitrogen" },
-        #p{},
-        "
-        If you can see this page, then your Nitrogen server is up and
-        running. Click the button below to test postbacks.
-        ",
-        #p{}, 	
-        #button { id=button, text="Click me!", postback=click },
-        #p{},
-        "
-        Run <b>./bin/dev help</b> to see some useful developer commands.
-        "
+        #h1 { text="Dashboard" },
+        #span { style="position: absolute; top: 4px; right: 4px;", body=[
+            " Welcome, ", #span { class=description, text=wf:user() },
+            #button { text="Logout", postback=logout },
+            " (", iso_8601_fmt(erlang:localtime()), ") "
+        ]},
+        inner_body()
     ].
-	
-event(click) ->
-    wf:replace(button, #panel { 
-        body="You clicked the button!", 
-        actions=#effect { effect=highlight }
-    }).
+
+inner_body() ->
+    [
+        #p{}, "Hello, here's the scoop"
+    ].
+
+event(logout) ->
+    wf:logout(),
+    wf:redirect_to_login("/login").
